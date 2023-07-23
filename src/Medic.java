a9000a272624c806b493b353b689d7a6079a299a
ZhannaSap <nekotyan474@gmail.com>
2023-07-23 17:45:54 +0600

    Initial commit

---
+++ src/Medic.java
@@ -0,0 +1,9 @@
+public class Medic extends Hero{
+    private int healPoints;
+
+    @Override
+    public void applySuperAbility() {
+        System.out.println("Medic  has applied the INSTANT HEALING superpower.");
+    }
+    
+}
